c2935dfa8ac32a6de6b2efa240568340c1938f24
Adam Ackerman <adamackrmn@gmail.com>
2019-01-27 23:30:28 -0600

    Initial creation of all projects + started code

---
+++ Homework1/Collections-Arrays-2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections_Arrays_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] lowercaseLetters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+        }
+    }
+}
